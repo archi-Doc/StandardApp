9c84aa5a3866465359fbe750f4f8c1a2eaf4e593
archi-Doc <archidoc422@gmail.com>
2021-06-19 01:00:38 +0900

    Add StandardConsole

---
+++ StandardConsole/Program.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Threading.Tasks;
+using SimpleCommandLine;
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+
+namespace StandardConsole
+{
+    public class TestOptions
+    {
+        [SimpleOption("number", "n")]
+        public int Number { get; set; } = 10;
+    }
+
+    [SimpleCommand("test")]
+    public class TestCommand : ISimpleCommandAsync<TestOptions>
+    {
+        public async Task Run(TestOptions option, string[] args)
+        {
+            Console.WriteLine("Test command:");
+            Console.WriteLine($"Number is {option.Number}");
+        }
+    }
+
+    [SimpleCommand("test2")]
+    public class TestCommand2 : ISimpleCommand
+    {
+        public void Run(string[] args)
+        {
+            Console.WriteLine("Test command2:");
+        }
+    }
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var commandTypes = new Type[]
+            {
+                typeof(TestCommand),
+                typeof(TestCommand2),
+            };
+
+            await SimpleParser.ParseAndRunAsync(commandTypes, args);
+        }
+    }
+}
