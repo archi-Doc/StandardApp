6d71380e0a851251da85ab355b25957e860f8022
archi-Doc <archidoc422@gmail.com>
2021-06-19 18:52:34 +0900

    .

---
+++ StandardConsole/AppService.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.IO;
+using System.Threading;
+using Serilog;
+
+namespace StandardConsole
+{
+    public interface IAppService
+    {
+        void EnterCommand(string directory);
+
+        void ExitCommand();
+
+        void Terminate();
+
+        CancellationToken CancellationToken { get; }
+    }
+
+    public class AppService : IAppService
+    {
+        public void EnterCommand(string directory)
+        {
+            // Logger: Debug, Information, Warning, Error, Fatal
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .WriteTo.File(
+                Path.Combine(directory, "logs", "log.txt"),
+                rollingInterval: RollingInterval.Day,
+                retainedFileCountLimit: 31,
+                buffered: true,
+                flushToDiskInterval: TimeSpan.FromMilliseconds(1000))
+            .CreateLogger();
+
+            Log.Information("started");
+        }
+
+        public void ExitCommand()
+        {
+            Log.Information("terminated");
+            Log.CloseAndFlush();
+        }
+
+        public void Terminate()
+        {
+            this.cancellationTokenSource.Cancel();
+        }
+
+        public CancellationToken CancellationToken => this.cancellationTokenSource.Token;
+
+        private CancellationTokenSource cancellationTokenSource = new();
+    }
+}
--- StandardConsole/Program.cs
+++ StandardConsole/Program.cs
@@ -1,7 +1,11 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
 using System;
+using System.Threading;
 using System.Threading.Tasks;
+using DryIoc;
+using ImTools;
+using Serilog;
 using SimpleCommandLine;
 
 #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
@@ -11,17 +15,35 @@ namespace StandardConsole
     public class TestOptions
     {
         [SimpleOption("number", "n")]
-        public int Number { get; set; } = 10;
+        public int Number { get; set; } = 3000;
     }
 
     [SimpleCommand("test")]
     public class TestCommand : ISimpleCommandAsync<TestOptions>
     {
+        public TestCommand(IAppService appService)
+        {
+            this.AppService = appService;
+        }
+
         public async Task Run(TestOptions option, string[] args)
         {
+            this.AppService.EnterCommand(string.Empty);
+
             Console.WriteLine("Test command:");
             Console.WriteLine($"Number is {option.Number}");
+            try
+            {
+                await Task.Delay(option.Number, this.AppService.CancellationToken);
+            }
+            catch
+            {
+            }
+
+            this.AppService.ExitCommand();
         }
+
+        public IAppService AppService { get; }
     }
 
     [SimpleCommand("test2")]
@@ -35,15 +57,46 @@ namespace StandardConsole
 
     public class Program
     {
+        public static Container Container { get; } = new();
+
         public static async Task Main(string[] args)
         {
+            // Simple Commands
             var commandTypes = new Type[]
             {
                 typeof(TestCommand),
                 typeof(TestCommand2),
             };
 
-            await SimpleParser.ParseAndRunAsync(commandTypes, args);
+            // DI Container
+            Container.Register<IAppService, AppService>(Reuse.Singleton);
+            foreach (var x in commandTypes)
+            {
+                Container.Register(x, Reuse.Singleton);
+            }
+
+            Container.ValidateAndThrow();
+
+            AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+            {
+                Log.Information("exit (ProcessExit)");
+                Container.Resolve<IAppService>().Terminate();
+            };
+
+            Console.CancelKeyPress += (s, e) =>
+            {
+                Log.Information("exit (Ctrl+C)");
+                Container.Resolve<IAppService>().Terminate();
+            };
+
+            var parserOptions = SimpleParserOptions.Standard with
+            {
+                ServiceProvider = Container,
+                RequireStrictCommandName = false,
+                RequireStrictOptionName = true
+            };
+
+            await SimpleParser.ParseAndRunAsync(commandTypes, args, parserOptions);
         }
     }
 }
