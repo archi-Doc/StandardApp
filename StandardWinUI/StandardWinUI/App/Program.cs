6036941950e0010be53caab91d863ea5d734e9b8
archi-Doc <archidoc422@gmail.com>
2024-08-02 20:31:33 +0900

    .

---
+++ StandardWinUI/StandardWinUI/App/Program.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace StandardWinUI;
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+
+#if DISABLE_XAML_GENERATED_MAIN
+
+public static partial class Program
+{
+    [LibraryImport("Microsoft.ui.xaml.dll")]
+    private static partial void XamlCheckProcessRequirements();
+
+    #region FieldAndProperty
+
+    private static Mutex appMutex = new(false, AppConstants.MutexName);
+
+    #endregion
+
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        XamlCheckProcessRequirements();
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+        Application.Start(_ =>
+        {
+            try
+            {
+                var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+                SynchronizationContext.SetSynchronizationContext(context);
+                var app = new App();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error in application start callback: {ex.Message}.");
+            }
+        });
+    }
+}
+#endif
