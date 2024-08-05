80d3c52eef777a63bc329621ba46d2e7f038bbe2
archi-Doc <archidoc422@gmail.com>
2024-08-05 16:53:13 +0900

    .

--- StandardWinUI/StandardWinUI/Views/MainWindow.xaml.cs
+++ StandardWinUI/StandardWinUI/Views/MainWindow.xaml.cs
@@ -1,18 +1,21 @@
 // Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
 using System.Net.Sockets;
+using System.Threading.Tasks;
 using Arc.Views;
 using CommunityToolkit.WinUI;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 using StandardWinUI.ViewModels;
+using WinUIEx;
 
 namespace StandardWinUI.Views;
 
 /// <summary>
 /// An empty window that can be used on its own or navigated to within a Frame.
 /// </summary>
-public sealed partial class MainWindow : Window
+public sealed partial class MainWindow : WinUIEx.WindowEx
 {
     internal MainViewModel ViewModel { get; }
 
@@ -22,7 +25,7 @@ public sealed partial class MainWindow : Window
         this.InitializeComponent();
     }
 
-    private void myButton_Click(object sender, RoutedEventArgs e)
+    private async void myButton_Click(object sender, RoutedEventArgs e)
     {
         /*var children = ((FrameworkElement)this.Content).FindChildren();
         foreach (var x in children)
@@ -35,6 +38,22 @@ public sealed partial class MainWindow : Window
         {
             var child = VisualTreeHelper.GetChild(content, i);
         }*/
+        // await this.ShowMessageDialogAsync("test", "test2");
+        this.IsMaximizable = false;
         this.myButton.Content = "Clicked";
+        // this.Content.Scale = new(2);
+
+        // var grid = (Grid)this.Content;
+        // grid.Scale = new(2);
+
+        if (this.Content is FrameworkElement element)
+        {
+            if (element.FindChild<Viewbox>() is { } viewbox)
+            {
+                viewbox.Stretch = Stretch.Uniform;
+                viewbox.Width = viewbox.ActualWidth * 1.5;
+                viewbox.Height = viewbox.ActualHeight * 1.5;
+            }
+        }
     }
 }
