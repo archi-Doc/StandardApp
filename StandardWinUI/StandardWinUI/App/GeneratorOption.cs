fff3f70d15a9790cfa0dcf1deab604c943033766
archi-Doc <archidoc422@gmail.com>
2024-08-06 15:41:41 +0900

    .

--- StandardWinUI/StandardWinUI/App/GeneratorOption.cs
+++ StandardWinUI/StandardWinUI/App/GeneratorOption.cs
@@ -1,7 +1,5 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
-using Tinyhand;
-
 namespace StandardWinUI;
 
 [TinyhandGeneratorOption(AttachDebugger = false, GenerateToFile = false)]
