650e25ec8d87d41df9cd92367e506011bd41dd90
archi-Doc <archidoc422@gmail.com>
2026-04-24 00:27:22 +0900

    .

--- StandardConsole/Program.cs
+++ StandardConsole/Program.cs
@@ -37,7 +37,7 @@ public class Program
         await unit.RunAsync(new(args));
 
         ThreadCore.Root.Terminate();
-        await ThreadCore.Root.WaitForTerminationAsync(-1); // Wait for the termination infinitely.
+        await ThreadCore.Root.WaitForTermination(); // Wait for the termination infinitely.
         unit.Context.ServiceProvider.GetService<LogUnit>()?.FlushAndTerminate();
         ThreadCore.Root.TerminationEvent.Set(); // The termination process is complete (#1).
     }
